ea625777967914454eab4eb0581efb01b50e084e
Nataliia S <natalilu1990@gmail.com>
2024-10-18 01:51:42 +0300

    Виправлено шлях до main.jsx

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/react-estate-ui/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: './index.html', 
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src',  
+    },
+  },
 })

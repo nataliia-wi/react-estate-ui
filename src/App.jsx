8674851ed4b79405cb6067918d33bf4690fa5cdd
Nataliia S <natalilu1990@gmail.com>
2024-10-19 00:27:22 +0300

    Виправлено

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import HomePage from './routes/homePage/homePage'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from './routes/layout/layout';
 import ListPage from './routes/listPage/listPage';
 import SinglePage from './routes/singlePage/singlePage';
 import ProfilePage from "./routes/profilePage/profilePage";
 import Login from "./routes/login/login";
 import Register from "./routes/register/register";
-import { createHashRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-  const router = createHashRouter([
+  const router = createBrowserRouter([
     {
       path: "/",
       element: <Layout />,
@@ -39,7 +39,9 @@ function App() {
         }
       ]
     }
-  ]);
+  ], {
+    basename: "/react-estate-ui"
+  });
 
   return (
 
--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-estate-ui/',
+  base: '/react-estate-ui',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: './index.html', 
-    },
-  },
-  resolve: {
-    alias: {
-      '@': '/src',  
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     input: './index.html', 
+  //   },
+  // },
+  // resolve: {
+  //   alias: {
+  //     '@': '/src',  
+  //   },
+  // },
 })
